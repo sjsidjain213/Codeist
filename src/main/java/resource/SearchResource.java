5cbc4cb9bc9d23be64ad704aff680ca98f585fa8
DELL\sid jain <sjsidjain213@gmail.com>
2017-03-18 20:29:00 +0530

    update searchresource

--- src/main/java/resource/SearchResource.java
+++ src/main/java/resource/SearchResource.java
@@ -1,5 +1,18 @@
 package resource;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/search")
 public class SearchResource {
 
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void search()
+	{
+		
+	}
+	
 }
