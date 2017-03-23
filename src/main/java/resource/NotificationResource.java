de8a657187008ece72d256f645465fdc1dc3dd86
DELL\sid jain <sjsidjain213@gmail.com>
2017-03-23 13:53:59 +0530

    Compile Time error Removed

--- src/main/java/resource/NotificationResource.java
+++ src/main/java/resource/NotificationResource.java
@@ -2,10 +2,12 @@ package resource;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.bson.Document;
@@ -17,10 +19,10 @@ import service.NotificationService;
 public class NotificationResource {
 
 @GET
-@Path("/retrieveall/{username}")
+@Path("/retrieveall")
 @Produces(MediaType.APPLICATION_JSON)
-public ArrayList<NotificationBean> getAllNotifcation(@PathParam("username") String username)
+public ArrayList<NotificationBean> getAllNotifcation(@Context HttpServletRequest req)
 {
-  return new NotificationService().getAllNotifications(username);
+  return new NotificationService().getAllNotifications(req.getSession().getAttribute("username").toString(),req.getSession().getAttribute("s_id").toString());
 }
 }
