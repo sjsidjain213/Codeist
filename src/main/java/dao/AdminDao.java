fedee6afa8e26584f1b669d4d2404febe9081ed6
Unknown <madaan1996@gmail.com>
2017-04-02 02:36:04 +0530

    edrfgn

--- src/main/java/dao/AdminDao.java
+++ src/main/java/dao/AdminDao.java
@@ -12,8 +12,17 @@ import service.GeneralServices;
 public class AdminDao {
 	MongoCollection <Document> tc = new DatabaseServices().getDb().getCollection("institute");
 	public Acknowledgement insertinstitute(Institute institute){
+		
+		
 		Document doc=new Document().append("loginid", institute.getLoginid())
-				.append("password", institute.getPassword());
+				.append("password", institute.getPassword())
+				.append("university_name", institute.getUniversity_name())
+				.append("college_name", institute.getCollege_name())
+				.append("state", institute.getState())
+				.append("city", institute.getCity())
+				.append("email_id",institute.getEmail_id())
+				.append("phone_no", institute.getPhone_no())
+				.append("address", institute.getAddress());
 				
 		return new GeneralServices().response(null);
 	}
