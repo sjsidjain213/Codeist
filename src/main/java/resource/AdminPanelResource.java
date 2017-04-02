1877294a0d2019895d2c2210ddc0f83081f30b96
DELL\sid jain <sjsidjain213@gmail.com>
2017-04-02 10:31:22 +0530

    update

--- src/main/java/resource/AdminPanelResource.java
+++ src/main/java/resource/AdminPanelResource.java
@@ -1,14 +1,17 @@
 package resource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import bean.Acknowledgement;
 import bean.Institute;
 import dao.AdminDao;
+import service.GeneralServices;
 
 @Path("/adminpanel")
 public class AdminPanelResource {
@@ -18,5 +21,13 @@ public class AdminPanelResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Acknowledgement insertinstitute(Institute institute){
 		return new AdminDao().insertinstitute(institute);
+	}
+
+	@GET
+	@Path("/rating/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Long getRating(Institute institute, @PathParam("username") String username){
+		return new GeneralServices().getRating(username);
 	}
 }
